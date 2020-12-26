60b83d8ccc2ee6c589ae2d5dec9747c558906d31
SMNik2546 <53541454+SMNik2546@users.noreply.github.com>
2020-12-26 02:51:51 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,22 @@
+import numpy as np
 import unittest
-from project_1 import Molecule, a, b
-import sympy as smp
+import project1
+#import Benzene
 
+class Test(unittest.TestCase):
+    def test_1(self):
+        assert project1.Molecule
+        assert project1.Molecule.find_bond_order
+        assert project1.Molecule.find_charge_density
+        assert project1.Molecule.set_constants
+        assert project1.Molecule.generate_eigen
+        assert project1.Molecule.energy_level_plot
+        assert project1.Molecule.find_deloc_energy
+        assert project1.Molecule.energy_level_plot
+        assert project1.Molecule.add_connections
+        assert project1.Molecule.generate_H
+        assert project1.Molecule.find_nodes
+        assert project1.Molecule.delete_connections
+        assert project1.Molecule.__str__
+        assert project1.Molecule.__init__
+        
